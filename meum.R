9ee9ee15a7c6688b18756e43711b52eb0de1a014
Michał Kruszewski <mkru@protonmail.com>
2018-12-29 16:36:53 +0100

    Minor prepare for XGBoost.

--- meum.R
+++ meum.R
@@ -1,5 +1,9 @@
+library(TTR)
+
 NUMBER_OF_NN3_TIME_SERIES = 111
 TEST_DATA_LENGTH = 18
+
+DEBUG = TRUE
 
 input_time_series = read.csv('input.csv', header = F)
 
@@ -11,7 +15,25 @@ if (length(input_time_series) != NUMBER_OF_NN3_TIME_SERIES) {
 input_time_series = matrix(unlist(input_time_series, use.names = F),
                            ncol = NUMBER_OF_NN3_TIME_SERIES)
 
-for (i in 1:NUMBER_OF_NN3_TIME_SERIES) {
-    tmp_time_series = na.omit(input_time_series[,i])
-    test_data = tail(tmp_time_series, TEST_DATA_LENGTH)
+#for (i in 1:NUMBER_OF_NN3_TIME_SERIES) {
+for (i in 1:1) {
+    tmp_ts = na.omit(input_time_series[,i])
+
+    # Create input data features for XGBoost
+    month_in_year = 0:length(tmp_ts) %% 12
+
+    # Relative strength index
+    rsi = RSI(tmp_ts, n = 3, maType = "WMA")
+    # Potential need to create lag in the features to avoid look-ahead bias?
+
+    input_data = cbind(tmp_ts, month_in_year, rsi)
+    colnames(input_data) = c("Data", "Month", "RSI")
+
+    if (DEBUG) {
+        print(input_data)
+    }
+
+    learn_data_length = length(tmp_ts) - TEST_DATA_LENGTH
+    learn_data = head(tmp_ts, learn_data_length)
+    test_data = tail(tmp_ts, TEST_DATA_LENGTH)
 }
