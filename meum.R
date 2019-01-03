11e9f4ce6f5bca536c124e2a1fa421338e3c0bc6
Michał Kruszewski <mkru@protonmail.com>
2019-01-03 11:50:27 +0100

    Create skeleton for xgboost forecasting.

--- meum.R
+++ meum.R
@@ -1,4 +1,5 @@
 library(TTR)
+library(xgboost)
 
 NUMBER_OF_NN3_TIME_SERIES = 111
 TEST_DATA_LENGTH = 18
@@ -16,11 +17,11 @@ input_time_series = matrix(unlist(input_time_series, use.names = F),
                            ncol = NUMBER_OF_NN3_TIME_SERIES)
 
 #for (i in 1:NUMBER_OF_NN3_TIME_SERIES) {
-for (i in 1:1) {
+for (i in 1:2) {
     tmp_ts = na.omit(input_time_series[,i])
 
     # Create input data features for XGBoost
-    month_in_year = 0:length(tmp_ts) %% 12
+    month_in_year = 0:(length(tmp_ts)-1) %% 12
 
     # Relative strength index
     rsi = RSI(tmp_ts, n = 3, maType = "WMA")
@@ -34,6 +35,23 @@ for (i in 1:1) {
     }
 
     learn_data_length = length(tmp_ts) - TEST_DATA_LENGTH
-    learn_data = head(tmp_ts, learn_data_length)
-    test_data = tail(tmp_ts, TEST_DATA_LENGTH)
+    learn_data = input_data[1:learn_data_length,]
+    test_data = input_data[(learn_data_length+1):nrow(input_data),]
+
+    # Split data training and test data into X and Y
+    x_learn = learn_data[,2:ncol(input_data)]
+    y_learn = learn_data[,1]
+    x_test = test_data[,2:ncol(input_data)]
+    y_test = test_data[,1]
+
+    xgb_model = xgboost(data=x_learn, label=y_learn, nround=5, objective="reg:linear")
+    xgb_forecast = predict(xgb_model, x_test)
+
+    xgb_error = (xgb_forecast - y_test) / y_test
+
+    if (exists("xgb_relative_error")) {
+        xgb_relative_error = cbind(xgb_relative_error, xgb_error)
+    } else {
+        xgb_relative_error = cbind(xgb_error)
+    }
 }
